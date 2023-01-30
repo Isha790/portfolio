ccf3d1d47b4cef73d5b75f0b39723983e856f2a6
Isha Khatri <khatriisha79@gmail.com>
2023-01-30 21:28:54 +0530

    initial-commit

---
+++ src/resumeData.js
@@ -0,0 +1,104 @@
+let resumeData = {
+    "imagebaseurl":"file:///C:/Users/hp/Downloads/isha79.pdf",
+    "name": "Isha Khatri",
+    "role": "Frontend Developer ",
+    "linkedinId":"Isha Khatri",
+    "instagramId":"khatri_isha",
+    
+    
+    "roleDescription": "I like to make website and  like to learn about new technologies or simply do sketching in my free times .",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/isha-khatri-ab2796202",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Isha790",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at Lovely Professional University and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Android development. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "mail":"khatriisha**@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Lovely Professional University",
+        "specialization":"Web development",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2024",
+        "Achievements":"Participated in Hackathon",
+      }
+      
+    ],
+    
+    "skillsDescription":"My skills ",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+
+      {
+        "skillname":"Python"
+      },
+
+      {
+        "skillname":"SQL"
+      }, {
+        "skillname":"Reactjs"
+      },
+
+      {
+        "skillname": "Excel"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"Digitalcalender",
+        "imgurl":"https://codescracker.com/python/images/display-all-months-calendar-of-given-year-python.JPG",
+
+        
+      },
+
+      {
+        "name":"project2",
+        "description":"fleet Management Dashboard",
+        "imgurl": "https://advancedfleetmanagementconsulting.com/eng/wp-content/uploads/sites/3/2020/04/saferdrive-dashboard-fleet-performance-1.jpg"
+        
+        
+      },
+      {
+        "name":"project3",
+        "description":"dice roller", 
+        "imgurl": "https://miro.medium.com/max/828/1*AwbFFUngdnLHOCkbnUvfog.webp"
+      
+      },
+      {
+        "name":"project4",
+        "description":"Blog website  ",
+        "imgurl":"https://cdn4.wpbeginner.com/wp-content/uploads/2018/07/blogposts.png"
+        
+      }
+   ],
+
+    "work": [
+        {
+            "CompanyName" : "ISDRR NGO",
+            "specialization":"Empowerment of Women",
+            "MonthOfLeaving":"June",
+            "Serving Period": "6 Month",
+            "YearOfLeaving" : "2021",
+            "Achievements": "Certificate"
+        }
+    ],
+        
+    
+     
+  }
+  
+  export default resumeData
